4994ff55c53095881271524c236e5e485bf4e0a7
Ankit Sagwekar <ankit.sagwekar15@gmail.com>
2016-02-29 00:41:44 +0530

    Update moodle.py

--- moodle.py
+++ moodle.py
@@ -4,19 +4,12 @@ from bs4 import BeautifulSoup
 
 
 def main(text):
-<<<<<<< HEAD
-   
-=======
-    #print text
->>>>>>> origin/master
+
     mylist = text.split(',')
     username = mylist[0]
     password = mylist[1]
     batch=mylist[2].upper()
-<<<<<<< HEAD
-    
-=======
->>>>>>> origin/master
+
     URL = 'http://moodle.spit.ac.in/login/index.php'
 
     login_data = {
@@ -29,19 +22,11 @@ def main(text):
     s.post(URL, login_data)
 
     r = s.get('http://moodle.spit.ac.in/calendar/view.php', timeout=5)
-<<<<<<< HEAD
-    
-=======
->>>>>>> origin/master
 
     soup = BeautifulSoup((r.text).encode('utf-8'),"lxml")
 
     soup = soup.findAll('table', {"class": "event"})
-<<<<<<< HEAD
-   
-=======
-    # print(str(soup))
->>>>>>> origin/master
+
     i = 0
     sr = ''
     soup = str(soup).replace("\\n", "")
@@ -55,11 +40,7 @@ def main(text):
     sr = sr.replace(',', ' ')
     sr = sr.replace('[', ' ')
     sr = sr.replace(']', ' ')
-<<<<<<< HEAD
-   
-=======
-    
->>>>>>> origin/master
+
     flag = 1
     pr = 0
     final = ''
@@ -74,26 +55,18 @@ def main(text):
         else:
 
             if flag == 1:
-<<<<<<< HEAD
-                #print line
-=======
-                
->>>>>>> origin/master
+
                 if 'Batch' in line or 'batch' in line:
                     rbatch = line.replace('-', ' ')
                     rbatch = rbatch.replace('.', ' ')
                     
                     wordList = re.sub("[^\w]", " ",  rbatch).split()
-<<<<<<< HEAD
-                    
-=======
-                   
->>>>>>> origin/master
+
                     for a in wordList:
 
                         if len(a)==1:
                             a=a.upper()
-<<<<<<< HEAD
+
                            
                             if batch in a or a in batch:
                                 
@@ -101,7 +74,7 @@ def main(text):
                                
                                 final = final + '\n -------------------------------------------------------\n'
                            
-=======
+
                             
                             if batch in a or a in batch:
                                 
@@ -109,7 +82,7 @@ def main(text):
                                 
                                 final = final + '\n -------------------------------------------------------\n'
                             
->>>>>>> origin/master
+
                             else:
                                 pr = 0
 
@@ -119,14 +92,11 @@ def main(text):
                 flag = 0
             if pr == 1:
                 final = final + '\n' + str(line)
-<<<<<<< HEAD
-    return str(final)
-   
-=======
+
     
     return str(final)
     
->>>>>>> origin/master
+
 
 
 
