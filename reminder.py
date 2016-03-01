f665961d67fad35c16ce22bb9a699248ac759c53
ankit96 <ankit.sagwekar@yahoo.co.in>
2016-03-01 06:53:42 +0530

    added error handling

--- reminder.py
+++ reminder.py
@@ -7,18 +7,16 @@ from slackclient import SlackClient
 import slackweb
 
 
-slack = slackweb.Slack(url="i_wont_give_you_my_url_:|")
-
+slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
 
 
 def remind():
      conn = psycopg2.connect(
-
             database='a',
-            user='a',
+            user='b',
             password='a',
             host='p',
-            port='r'
+            port='a'
 
         )
      cur = conn.cursor()
@@ -26,26 +24,34 @@ def remind():
      cur.execute("SELECT * from users")
      rows = cur.fetchall()
 
+     
+
+
      for row in rows:
          mylist = str(row[2]).split(',')
          username = mylist[0]
          encrpyt = mylist[1]
-         # decrypt= encrypt + x
-         password = decrypt
+         
+         password = #decrypting encrypt
          batch=mylist[2].upper()
          string=username+","+password+","+batch
          value=main(str(string))
          
          userid=str(row[0])
          username=str(row[1])
+        
          data = "<@"
          data = data.strip()+str(userid)
          data = data.strip()+'|'
          data = data.strip()+str(username)
          data = data.strip()+'>'
-         data = data.strip()+':Your comming submission deadlines'+'\n'+str(value)
-        
+         if 'Invalid' in data and 'down' in data:
+            data = data.strip()+'\n'+str(value)
+
+         else:
+            data = data.strip()+':Your upcoming submission deadlines'+'\n'+str(value)
          channelname="@"+username
          slack.notify(text=data, channel=channelname, username="moodler")
+         
      conn.commit()
      conn.close()
